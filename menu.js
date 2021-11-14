f3203a94524c42e47398c0d2909e3c49d2fd56d0
krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2021-11-15 02:19:22 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.menuBurger').on('click', function(){
-    $('.menu').slideToggle(200, function(){
+    $('.menu_bar').slideToggle(200, function(){
       if($(this).css('display') === 'none'){
       $(this).removeAttr('style');
   }
