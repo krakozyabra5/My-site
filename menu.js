63c7ac732f2f9becf90bf737b2b6ef55af882f77
krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2021-11-15 02:17:44 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,10 +1,9 @@
- let menu = document.querySelector('.menuBurger')
- let menu_bar = document.querySelector('.menu_bar')
- let menu_mobile = document.querySelector('.menu')
- let nav_mobile = document.querySelector('.nav')
- menu.onclick = function (){
-  menu_bar.classList.toggle('menu_bar')
-  menu_bar.classList.toggle('menu_bar_mobile')
-  menu_mobile.classList.toggle('menu_mobile')
-  nav_mobile.classList.toggle('nav_mobile')
- }
+$(function(){
+  $('.menuBurger').on('click', function(){
+    $('.menu').slideToggle(200, function(){
+      if($(this).css('display') === 'none'){
+      $(this).removeAttr('style');
+  }
+});
+});
+});
