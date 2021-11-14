421169ec73b4e5f82f31a06f27690d57e824f759
krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2021-11-15 00:56:59 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
  let menu = document.querySelector('.menuBurger')
  let menu_bar = document.querySelector('.menu_bar')
+ let menu = document.querySelector('.menu')
  menu.onclick = function (){
   menu_bar.classList.toggle('menu_bar')
   menu_bar.classList.toggle('menu_bar_mobile')
+  menu.classList.toggle('menu_mobile')
  }
