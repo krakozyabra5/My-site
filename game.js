12f5e1d4c4764821dad23ae2bb21d51f20f447e6
Krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2022-04-02 21:37:02 +0700

    Update game.js

--- game.js
+++ game.js
@@ -32,8 +32,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 //игровой цикл
-  function loop() {  
-    requestAnimationFrame(loop);
+function loop() {  
+  requestAnimationFrame(loop);
 //скорость обновления кадров
   if (count < max_count) {
     count = count + 1;
@@ -135,55 +135,55 @@ function getRandomInt(min, max) {
   }
 //добавляем клетку перед змейкой по координатам (поменяв x и y можно инвертировать управление относительно осей x и y)
   snake.cells.unshift({ x: snake.x, y: snake.y });
-if (snake.cells.length > snake.maxCells) {
+  if (snake.cells.length > snake.maxCells) {
 //удаляем последний элемент из массива змейки (лишнюю клетку хвоста)
-  snake.cells.pop();
-}
-//рисуем яблоко (по коодинатам)
-context.fillStyle = 'rgba(0, 0, 0, 0)';
-context.drawImage(image, 0, 0, 16, 16, apple.x, apple.y, 16, 16);
-context.fillRect(apple.x, apple.y, grid - 0, grid - 0);
-//рисуем ВСЕ клетки змейки (по коодинатам)
-context.fillStyle = 'yellow';
-snake.cells.forEach(function (cell, maxCells) {
-  context.fillRect(cell.x, cell.y, grid - 0, grid - 0);
-//если змейка добралась до яблока...
-  if (cell.x == apple.x & cell.y == apple.y) {
-//увеличиваем длину змейки
-    snake.maxCells = snake.maxCells + 1;
-//прибавляем 1 к счёту и обновляем его
-    score = score + 1;
-    document.querySelector('.score').textContent = 'Очки: ' + score;
-//генерим яблоко
-    apple.x = getRandomInt(1, 18) * grid; 
-    apple.y = getRandomInt(1, 27) * grid;
+    snake.cells.pop();
   }
-//проверяем, не столкнулась ли змейка сама с собой, сравнивая координаты всех её клеток
-  for (let i = maxCells + 1; i < snake.cells.length; i++) {
-//если такие клетки есть — начинаем игру заново
-    if (cell.x == snake.cells[i].x & cell.y == snake.cells[i].y) {
-//сравниваем текущий счёт и максимальный
-      if (max_score < score) {
-        max_score = score;
-//выводим новый максимальный счёт
-        document.querySelector('.max_score').textContent = 'Рекорд: ' + max_score;
-      }
-//обнуляем текущий счёт
-      score = 0;
+//рисуем яблоко (по коодинатам)
+  context.fillStyle = 'rgba(0, 0, 0, 0)';
+  context.drawImage(image, 0, 0, 16, 16, apple.x, apple.y, 16, 16);
+  context.fillRect(apple.x, apple.y, grid - 0, grid - 0);
+//рисуем ВСЕ клетки змейки (по коодинатам)
+  context.fillStyle = 'yellow';
+  snake.cells.forEach(function (cell, maxCells) {
+    context.fillRect(cell.x, cell.y, grid - 0, grid - 0);
+//если змейка добралась до яблока...
+    if (cell.x == apple.x & cell.y == apple.y) {
+//увеличиваем длину змейки
+      snake.maxCells = snake.maxCells + 1;
+//прибавляем 1 к счёту и обновляем его
+      score = score + 1;
       document.querySelector('.score').textContent = 'Очки: ' + score;
-//создаём новую змейку
-      snake.x = 144;
-      snake.y = 352;
-      snake.cells = [];
-      snake.maxCells = 6;
-      snake.dx = 0;
-      snake.dy = -grid;
 //генерим яблоко
-      apple.x = getRandomInt(1, 18) * grid;
+      apple.x = getRandomInt(1, 18) * grid; 
       apple.y = getRandomInt(1, 27) * grid;
     }
-  }
-});
+//проверяем, не столкнулась ли змейка сама с собой, сравнивая координаты всех её клеток
+    for (let i = maxCells + 1; i < snake.cells.length; i++) {
+//если такие клетки есть — начинаем игру заново
+      if (cell.x == snake.cells[i].x & cell.y == snake.cells[i].y) {
+//сравниваем текущий счёт и максимальный
+        if (max_score < score) {
+          max_score = score;
+//выводим новый максимальный счёт
+          document.querySelector('.max_score').textContent = 'Рекорд: ' + max_score;
+        }
+//обнуляем текущий счёт
+        score = 0;
+        document.querySelector('.score').textContent = 'Очки: ' + score;
+//создаём новую змейку
+        snake.x = 144;
+        snake.y = 352;
+        snake.cells = [];
+        snake.maxCells = 6;
+        snake.dx = 0;
+        snake.dy = -grid;
+//генерим яблоко
+        apple.x = getRandomInt(1, 18) * grid;
+        apple.y = getRandomInt(1, 27) * grid;
+      }
+    }
+  });
 }
 //регистрируем нажатие на стрелки (вторая часть условия проверки мешает двигаться "в себя")
 document.addEventListener('keydown', function (e) {
@@ -234,7 +234,7 @@ start_game.onclick = function () {
 
 function game() {
   if (start = 0) {
-    return;
+    function game()
   }
 } 
 
