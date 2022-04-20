cb05275592be227fd91343959dbbcad77476c651
Krakozyabra5 <91729303+krakozyabra5@users.noreply.github.com>
2022-04-20 19:50:04 +0700

    Update arrow.js

--- arrow.js
+++ arrow.js
@@ -7,7 +7,7 @@ arrow_next.onclick = function () {
   div_next.classList.toggle('hidden')
   div_previous.classList.toggle('hidden')
   window.scrollBy({
-    top: -window.innerHeight - 1000,
+    top: 0,
     behavior: 'smooth'
   })
 }
@@ -16,7 +16,7 @@ arrow_previous.onclick = function () {
   div_next.classList.toggle('hidden')
   div_previous.classList.toggle('hidden')
   window.scrollBy({
-    top: -window.innerHeight - 1000,
+    top: 0,
     behavior: 'smooth'
   })
 }
